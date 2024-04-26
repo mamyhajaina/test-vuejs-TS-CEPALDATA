bf824df5828d383bbd6d346c1579f358fe2730d0
hajainaHayramaTechnology <160156114+hajainaHayramaTechnology@users.noreply.github.com>
2024-04-26 16:36:19 +0200

    [Details]

--- src/components/Details.vue
+++ src/components/Details.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="row">
-    <div class="col-xl-4">
+  <div class="row justify-content-center">
+    <div class="col-xl-8">
 
       <div class="card">
         <div class="card-body profile-card pt-4 d-flex flex-column align-items-center">
