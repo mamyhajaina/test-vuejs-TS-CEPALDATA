55a38a696b8d70962f9881acfb6c3c9b2a39fa7c
hajainaHayramaTechnology <160156114+hajainaHayramaTechnology@users.noreply.github.com>
2024-04-26 13:16:37 +0200

    Initial commit

---
+++ src/components/Compagnies.vue
@@ -0,0 +1,45 @@
+<template>
+  <section class="section dashboard">
+    <div class="card" v-for="companie in companies">
+      <div class="d-flex align-items-center">
+        <img :src="companie.Logo" class="rounded-circle" alt="Profile">
+        <div class="ps-3">
+          <h6>{{ companie['Company Name'] }}</h6>
+          <span class="text-muted small pt-2 ps-1">{{ companie.About }}</span>
+    
+        </div>
+      </div>
+      <div class="card-footer">
+        <div class="row">
+          <div class="col-6">
+            Vue: {{ companie.vue }}
+          </div>
+          <div class="col-6">
+            <button type="button" class="btn btn-primary">Voir</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import axios from 'axios';
+import { defineProps, ref, onMounted } from 'vue';
+
+const companies = ref([]);
+
+onMounted(async () => {
+  try {
+    const response = await axios.get('https://retoolapi.dev/Oal4aL/listing-company');
+    companies.value = response.data;
+    console.log('response.data',response.data);
+  } catch (error) {
+    console.error('Une erreur s\'est produite:', error);
+  }
+});
+</script>
+
+
+<style scoped>
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,58 @@
+<template>
+  <section class="section">
+    <div class="row align-items-top">
+      <div class="col-lg-3">
+      </div>
+
+      <div class="col-lg-6">
+        <div class="pagetitle">
+          <nav>
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+              <li class="breadcrumb-item">Components</li>
+              <li class="breadcrumb-item active">Cards</li>
+            </ol>
+          </nav>
+        </div><!-- End Page Title -->
+        
+        <Compagnies></Compagnies>
+      </div>
+
+      <div class="col-lg-3">
+
+        
+        <!-- Special title treatmen -->
+        <div class="card text-center">
+          <div class="card-header">
+            <ul class="nav nav-pills card-header-pills">
+              <li class="nav-item">
+                <a class="nav-link active" href="#">Active</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+              </li>
+            </ul>
+          </div>
+          <div class="card-body">
+            <h5 class="card-title">Special title treatment</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div><!-- End Special title treatmen -->
+
+      </div>
+
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import Compagnies from '../components/Compagnies.vue'
+</script>
+
+<style>
+
+</style>
